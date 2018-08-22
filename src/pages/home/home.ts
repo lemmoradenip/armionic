5f02853b2d2f5c511e16ed654e13551b9ec4da9a
rommel-lt <rmmelpneda.myphone@gmail.com>
2018-08-22 17:40:35 +0300

    home page no db

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,44 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { Platform } from 'ionic-angular';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  items;
+  Segments: string = "Shops";
 
-  constructor(public navCtrl: NavController) {
 
+  constructor() {
+
+    this.initializeItems();
+  }
+  initializeItems() {
+    this.items = [
+      'List A',
+      'List B',
+      'List C',
+      'List D',
+      'List E',
+      
+    ];
+    
+  }
+
+  getItems(ev) {
+    // Reset items back to all of the items
+    this.initializeItems();
+
+    // set val to the value of the ev target
+    var val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.items = this.items.filter((item) => {
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+    }
   }
 
 }
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,8 +1,7 @@
 import { Component } from '@angular/core';
-
 import { AboutPage } from '../about/about';
 import { ContactPage } from '../contact/contact';
-import { HomePage } from '../home/home';
+import{HomePage} from '../home/home';
 
 @Component({
   templateUrl: 'tabs.html'
